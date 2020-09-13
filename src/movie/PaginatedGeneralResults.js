2ad19cfc96d394bde38d665b3c6af2772486375a
redzi <redzi44@gmail.com>
2020-09-13 16:49:24 +0200

    Integration with backend pt. two - use real data

--- src/movie/PaginatedGeneralResults.js
+++ src/movie/PaginatedGeneralResults.js
@@ -1,5 +1,4 @@
-import React from "react";
-import generalPaginatedResults from "./generalPaginatedResults";
+import React, {useEffect, useState} from "react";
 import GeneralResult from "./GeneralResult";
 import PageChanger from "./PageChanger";
 import "./css/PaginatedResults.css";
@@ -8,18 +7,24 @@ import connect from "./integration/RSocketClinet"
 
 function PaginatedGeneralResults (props) {
 
-    function getMockedGeneralPaginatedResults() {
+    const [result, setResult] = useState({
+        infos: []
+    })
 
-        connect();
+    useEffect(() => {
+        connect()
+            .then(data => setResult(data))
+    }, [])
 
-        return generalPaginatedResults
+    function prepareGeneralPaginatedResults() {
+        return result
             .infos
             .map(r => <GeneralResult key={r.imdbID} title={r.title} year={r.year} poster={r.poster} imdbID={r.imdbID} />)
     }
 
     return (
         <div className="paginated-results">
-            {getMockedGeneralPaginatedResults()}
+            {prepareGeneralPaginatedResults()}
             <PageChanger/>
         </div>
     );
